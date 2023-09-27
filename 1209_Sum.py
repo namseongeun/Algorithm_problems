fb7b7dea36b2a50012cbfa293a63cc8b9d9d0c60
namseongeun <myaeja200@naver.com>
2023-09-27 17:55:59 +0900

    SWEA_1209

---
+++ 1209_Sum.py
@@ -0,0 +1,30 @@
+import sys
+
+sys.stdin = open('1209_Sum.txt')
+
+for _ in range(10):
+    tc = int(input())
+    mat = [list(map(int, input().split())) for _ in range(100)]
+
+    max_sum = 0
+    diagonal_sum1 = 0
+    diagonal_sum2 = 0
+
+    for i in range(100):
+        diagonal_sum1 += mat[i][i]
+        diagonal_sum2 += mat[i][-i]
+
+        if sum(mat[i]) > max_sum:
+            max_sum = sum(mat[i])
+
+    # 전치
+    for i in range(100):
+        for j in range(100):
+            if i < j:
+                mat[i][j], mat[j][i] = mat[j][i], mat[i][j]
+
+    for i in range(100):
+        if sum(mat[i]) > max_sum:
+            max_sum = sum(mat[i])
+
+    print(f'#{tc} {max(max_sum, diagonal_sum1, diagonal_sum2)}')
