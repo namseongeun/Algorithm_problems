d82e10244fabfa6d855de55b12e5610106382d46
namseongeun <gumska95@gmail.com>
2023-10-16 18:32:43 +0900

    DFS

---
+++ DFS_code.py
@@ -0,0 +1,13 @@
+graph = {}
+
+def DFS(start_node):
+    visited = []
+    stack = [start_node]
+
+    while stack:
+        tmp = stack.pop()
+        if tmp not in visited:
+            visited.append(tmp)
+            for node in graph[tmp][::1]:
+                if node not in visited:
+                    stack.append(node)
