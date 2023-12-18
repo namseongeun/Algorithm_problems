9aad7b156d4566876a7e4da41bab83ee6f664ceb
namseongeun <gumska95@gmail.com>
2023-12-18 16:17:07 +0900

    SWEA_4874_resolve

--- 4874_Forth.py
+++ 4874_Forth.py
@@ -4,49 +4,34 @@ import sys
 sys.stdin = open('4874_Forth.txt')
 
 T = int(input())
-
 for tc in range(1, T+1):
-    arr = list(input().split())
+    str_lst = list(input().split())[:-1]
 
-    num_arr = []
-    symbol_arr = []
-    symbols = ['+', '-', '*', '/']
+    stack = []
+    for char in str_lst:
+        try:
+            if char not in "*/+-":
+                stack.append(int(char))
+            else:
+                x = stack.pop()     # 스택에서 가장 위에 있던거 (부호전에 있던 숫자중 뒷 숫자)
+                y = stack.pop()     # 스택에서 그 다음 위에 있던거 (부호전에 있던 숫자중 앞 숫자)
 
-    for elem in arr:
-        if elem in symbols:
-            symbol_arr.append(elem)
-        elif elem != '.':
-            num_arr.append(elem)
-    
+                # 부호가 등장하면 그에 따라서 그 앞에 있는 숫자들을 조작
+                if char == "+":
+                    stack.append(y + x)
+                elif char == "-":
+                    stack.append(y - x)
+                elif char == "*":
+                    stack.append(y * x)
+                elif char == "/":
+                    stack.append(y // x)
+        except:
+            result = "error"
+            break
 
-    if ( len(num_arr) - 1 ) != len(symbol_arr):
-        result = 'error'
+    if len(stack) != 1:
+        result = "error"
+    else:
+        result = stack[0]
 
-    elif ( len(num_arr) - 1 ) == len(symbol_arr):
-
-        while len(num_arr) != 1:
-
-            if len(num_arr) == 1:            
-                answer = num_arr[0]
-                break
-
-            else:   
-                A = int(num_arr.pop(0))
-                B = int(num_arr.pop(0))
-
-                symbol = symbol_arr.pop(0)
-
-                if symbol == '+':
-                    C = A + B
-                    num_arr.append(C)
-                elif symbol == '-':
-                    C = A - B
-                    num_arr.append(C)
-                elif symbol == '*':
-                    C = A * B
-                    num_arr.append(C)                
-                else:
-                    C = A / B
-                    num_arr.append(C)                
-
-    print(answer)
+    print(f'#{tc} {result}')
